d9c02573e99fbc370b65adf3e2295cbd8e612625
sadra138622 <88475575+sadra138622@users.noreply.github.com>
2021-08-16 12:09:09 +0430

    Add files via upload

---
+++ sadra1.py.py
@@ -0,0 +1,70 @@
+#name ="sadra"
+#family ="aktarshnas"
+#print (name + family)
+
+
+
+#name ="sadra"
+#family ="aktarshnas"
+#print ("hello" + name + family)
+
+
+
+
+#name = "sadra"
+#family = "aktarshnas"
+#print ("hello" + " " + family)
+
+
+
+
+
+#name ="sadra"
+#family = "aktarshnas"
+#average = "19.5"
+#print = ("hello" + " " + name + " " + family + " " + average)
+
+
+
+
+
+
+#name = "sadra"
+#family = "aktarshnas"
+#average = str(19.5)
+#print = ("hello" + " " + name + " " +family + " " + average)
+
+
+
+
+#name = "sadra"
+#family = input()
+#print = (name + " " + family)
+
+
+
+#name = input()
+#family = input()
+#print ("hello" + " " + name + " " + family)
+
+
+
+
+#print(2+3)
+
+#a = 2
+#b = 3
+#print (2+3)
+
+
+
+
+
+#a = 2
+#b = 3
+# c = (a + b)
+#print (c)
+
+
+
+
